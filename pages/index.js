549949a891cfb9843a6749ba8ada05077eb468c1
JakeOJeff <realjakeojeff@gmail.com>
2025-07-12 19:56:11 +0530

    Added Container Styles

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,29 @@
 import Link from "next/link";
 
-const btnTWStyle = "text-white rounded px-4 py-2"
+const btnTWStyle = "font-bold text-white rounded px-4 py-2"
 
 
 export default function Home() {
   return (
-    <main className="bg-gray-950 flex flex-col items-center justify-center h-screen">
+    <main className="bg-gray-950 flex flex-col items-center
+     justify-center h-screen bg-radial-[at_0%_0%]
+      from-gray-900 via-gray-700  to-gray-950 to-100%">
 
-      <h1 className="text-5xl font-bold">Stacks Against You</h1>
-      <h2 className="font-bold text-gray-300">Join or Host a Party!</h2>
+      <div>
+        <div className="gap-1 flex flex-col md:container md:mx-auto items-center justify-center 
+        bg-gray-900/25 backdrop-blur-lg p-8 rounded-2xl shadow-2xl w-full max-w-2xl border border-gray-700">
+          <h1 className="text-5xl font-bold">Stacks Against You</h1>
+          <h2 className="font-bold text-gray-300">Join or Host a Party!</h2>
 
-      <div className="flex justify-between gap-2 m-2">
-          <Link href="/host" className={`${btnTWStyle} bg-blue-600`}>Host a Party</Link>
-          <Link href="/join" className={`${btnTWStyle} bg-green-600`}>Join a Party</Link>
+          <div className="flex gap-2 m-2">
+            <Link href="/host" className={`${btnTWStyle} bg-gray-700 hover:bg-[url('/diamonds-design.png')] bg-cover bg-center transition-all duration-500`}>Host Party</Link>
+            <Link href="/join" className={`${btnTWStyle} bg-gray-800`}>Join Party</Link>
+          </div>
+        </div>
 
       </div>
 
-    </main>    
+
+    </main>
   );
 }
