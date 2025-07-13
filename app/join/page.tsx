f123580efdcf65419b142cabd90a6313b570082f
JakeOJeff <realjakeojeff@gmail.com>
2025-07-14 02:21:11 +0530

    Added Join Style

--- app/join/page.tsx
+++ app/join/page.tsx
@@ -4,6 +4,12 @@ import { setUserSession } from "@/lib/userSession";
 import { socket } from "@/lib/socketClient";
 import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
+const btnStyle = {
+    container: "relative w-40 sm:w-48 h-12 overflow-hidden rounded-3xl group",
+    background: "absolute inset-0 transition-all duration-500 rounded-3xl",
+    image: "absolute inset-0 bg-[url('/diamonds-design.png')] bg-contain translate-y-full group-hover:translate-y-0 transition-transform duration-700 ease-in-out rounded-3xl",
+    link: "relative z-10 w-full h-full flex items-center justify-center font-bold text-white px-4 py-2  transition-colors duration-500 rounded-3xl",
+};
 
 export default function Join() {
     const [room, setRoom] = useState("");
@@ -28,27 +34,34 @@ export default function Join() {
     }, []);
 
     return (
-        <main className="flex flex-col items-center justify-center min-h-screen gap-2">
-            <input
-                type="text"
-                placeholder="Enter Username"
-                value={userName}
-                onChange={(e) => setUserName(e.target.value)}
-                className="w-64 px-4 py-2 mb-2 border-2 rounded-lg"
-            />
-            <input
-                type="text"
-                placeholder="Enter Room ID"
-                value={room}
-                onChange={(e) => setRoom(e.target.value)}
-                className="w-64 px-4 py-2 mb-4 border-2 rounded-lg"
-            />
-            <button
-                onClick={handleJoinRoom}
-                className="px-4 py-2 text-white bg-blue-500 rounded-lg"
-            >
-                Join Room
-            </button>
+        <main className="flex flex-col items-center justify-center h-screen bg-gray-950 bg-radial-[at_0%_0%] from-gray-900 via-gray-500 to-gray-950">
+            <div><div className="flex flex-col p-8 sm:p-12 md:container items-center bg-gray-900/25 backdrop-blur-lg rounded-2xl shadow-2xl border border-gray-700 text-center">
+                <input
+                    type="text"
+                    placeholder="Enter Username"
+                    value={userName}
+                    onChange={(e) => setUserName(e.target.value)}
+                    className="rounded-t-3xl rounded-b-2xl text-white border-white w-64 px-4 py-2 mb-2 border-2 "
+                />
+                <input
+                    type="text"
+                    placeholder="Enter Room ID"
+                    value={room}
+                    onChange={(e) => setRoom(e.target.value)}
+                    className="rounded-b-3xl rounded-t-2xl text-white border-white w-64 px-4 py-2 mb-4 border-2 "
+                />
+
+                <div className={btnStyle.container}>
+                    <div className={btnStyle.background} />
+                    <div className={btnStyle.image} />
+                    <button
+                        onClick={handleJoinRoom}
+                        className={btnStyle.link}
+                    >Join Room
+                    </button>
+                </div>
+            </div>
+            </div>
         </main>
     );
 }
