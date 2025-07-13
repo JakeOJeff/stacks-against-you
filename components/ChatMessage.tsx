5ea9a05b3de88fd77a8ce44906f9c1c74924f031
JakeOJeff <realjakeojeff@gmail.com>
2025-07-14 00:25:44 +0530

    Chat Design #1

--- components/ChatMessage.tsx
+++ components/ChatMessage.tsx
@@ -12,12 +12,12 @@ const ChatMessage = ( {sender, message, isOwnMessage }: ChatMessageProps) => {
     <div className={`flex ${isSystemMessage ? "justify-center" : isOwnMessage ? "justify-end" : "justify-start"} mb-3`}>
       <div className={`max-w-xs px-4 py-2 rounded-lg ${
         isSystemMessage
-          ? "bg-gray-100"
+          ? "bg-white/25 "
           : isOwnMessage
-          ? "bg-blue-200 text-white"
-          : "bg-gray-100 text-black"
+          ? "bg-gray-900/50 backdrop-blur-lg rounded-2xl shadow-2xl border border-gray-600 text-white"
+          : "bg-gray-100/25 backdrop-blur-lg rounded-2xl shadow-2xl border border-gray-100 text-black"
       }`}>
-        {!isSystemMessage && <p className="text-sm font-bold">{sender}</p>}
+        {!isSystemMessage && <p className="text-lg font-bold ">{sender}</p>}
         <p>{message}</p>
       </div>
     </div>
