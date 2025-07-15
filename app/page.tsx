677a79fc584ff9f9694679accf5b282e4e7d1773
JakeOJeff <realjakeojeff@gmail.com>
2025-07-16 03:11:47 +0530

    Added Info Button

--- app/page.tsx
+++ app/page.tsx
@@ -13,14 +13,18 @@ export default function Home() {
   return (
  <main className="flex flex-col items-center justify-center h-screen bg-gray-950 bg-radial-[at_0%_0%] from-gray-900 via-gray-800 to-gray-950">
 
-        <div><div className="w-[80vw] flex flex-col p-8 sm:p-12 md:container  bg-gray-900/25 backdrop-blur-lg rounded-2xl shadow-2xl border border-gray-700 text-center">
+        <div>
+          <div className="w-[80vw] flex flex-col p-8 sm:p-12 md:container  bg-gray-900/25 backdrop-blur-lg rounded-2xl shadow-2xl border border-gray-700 text-center">
+                  <div className="flex justify-end items-end w-full ">
+                    <Link href="/info" className="animate-pulse text-white text-2xl font-bold px-3 py-1 rounded-2xl hover:bg-gray-800 transition-colors duration-200">?</Link>
+                  </div>
           <Image
             src="/logo.png"
             width={1000 / 1.4}
             height={220 / 1.4}
             alt="Main Logo" />
-          <h2 className="text-xl sm:text-2xl font-bold text-gray-300 mt-2">Join or Host a Party!</h2>
-          <h3 className="text-gray-400 mt-1">Prioritizing privacy over anything!</h3>
+          <h2 className="text-md sm:text-2xl font-bold text-gray-300 mt-2">Join or Host a Party!</h2>
+          <h3 className="text-gray-400 mt-1 text-sm sm:text-md">Prioritizing privacy over anything!</h3>
 
           <div className="flex gap-2 justify-center mt-6 flex-wrap">
             {[
