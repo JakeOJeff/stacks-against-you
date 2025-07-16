b7d325f072aff5c9ef12f812587280690307a2f1
JakeOJeff <realjakeojeff@gmail.com>
2025-07-16 17:46:00 +0530

    Linux Nested

---
+++ app/info/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+export default function Info() {
+
+
+    return (
+        <main className="flex flex-col items-center justify-center h-screen bg-gray-950 bg-radial-[at_0%_0%] from-gray-900 via-gray-800 to-gray-950">
+            <p className="text-white"> THE INFO PAGE IS STILL A WORK IN PROGRESS</p>
+                    <p className="text-white">FAQ SECTION</p>
+
+        </main>
+    )
+}
