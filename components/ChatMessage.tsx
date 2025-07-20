5443c94d06e16d84a5f10431082410aec4f01617
JakeOJeff <realjakeojeff@gmail.com>
2025-07-20 18:19:16 +0530

    Design Modifications and Fixes

--- components/ChatMessage.tsx
+++ components/ChatMessage.tsx
@@ -13,10 +13,10 @@ const ChatMessage = ( {sender, message, isOwnMessage }: ChatMessageProps) => {
     <div className={`flex ${isSystemMessage ? "justify-center" : isOwnMessage ? "justify-end" : "justify-start"} mb-3`}>
       <div className={`max-w-xs px-4 py-2 rounded-lg ${
         isSystemMessage
-          ? "bg-white/25 "
+          ? "bg-gray-900/25 text-white font-semibold "
           : isOwnMessage
-          ? "bg-gray-900/50 backdrop-blur-lg rounded-2xl shadow-2xl min-w-32 border text-right border-gray-600 text-white duration-300  hover:bg-gray-900 hover:animate-pulse"
-          : "bg-gray-100/50 backdrop-blur-lg rounded-2xl shadow-2xl min-w-32 border text-left border-gray-100 text-black duration-300 hover:bg-gray-100 hover:animate-pulse "
+          ? "bg-gray-900/50 backdrop-blur-lg rounded-2xl shadow-2xl min-w-32  text-right  text-white duration-300 border border-gray-900 hover:bg-gray-900 hover:animate-pulse"
+          : "bg-gray-800/40 backdrop-blur-md rounded-2xl shadow-xl min-w-32 text-left text-white/70 duration-300 border border-gray-800 hover:bg-gray-700/60 hover:animate-pulse"
       }`}>
         {!isSystemMessage && <p className="text-lg font-bold ">{sender}</p>}
         <p>{message}</p>
