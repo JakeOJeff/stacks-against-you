33aea2a2ad9c475007b81987d1aa275054ab2563
JakeOJeff <realjakeojeff@gmail.com>
2025-07-20 23:22:32 +0530

    Package Updates and More Button

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const btnStyle = {
   container: "relative w-40 sm:w-48 h-12 overflow-hidden rounded-3xl group",
   background: "absolute inset-0 transition-all duration-500 rounded-3xl",
   image: "absolute inset-0 bg-[url('/diamonds-design.png')] bg-contain translate-y-full group-hover:translate-y-0 transition-transform duration-700 ease-in-out rounded-3xl",
-  link: "relative z-10 w-full h-full flex items-center justify-center font-bold text-white px-4 py-2  transition-colors duration-500 rounded-3xl",
+  link: "relative z-10 w-full h-full flex items-center justify-center font-bold text-white dark:text-white px-4 py-2  transition-colors duration-500 rounded-3xl",
 };
 
 export default function Home() {
@@ -15,7 +15,8 @@ export default function Home() {
 
       <div>
         <Link href="https://github.com/jakeojeff/stacks-against-you" className="ml-2 font-bold text-white p-2 pl-4 pr-4 rounded-t-full  hover:bg-white hover:text-gray-950/50 duration-500">Git</Link>
-        <Link href="https://github.com/jakeojeff/stacks-against-you" className="ml-2 font-bold text-white p-2 pl-4 pr-4 rounded-t-full  hover:bg-amber-300 hover:text-gray-950/50 duration-500">Mode</Link>
+        <Link href="https://github.com/jakeojeff" className="ml-2 font-bold text-white p-2 pl-4 pr-4 rounded-t-full  hover:bg-amber-300 hover:text-gray-950/50 duration-500">More</Link>
+
         <div className="w-[80vw] flex flex-col p-8 sm:p-12 md:container  bg-gray-900/25 backdrop-blur-lg rounded-2xl shadow-2xl border border-gray-700 text-center">
           <div className="flex justify-end items-end w-full ">
             <Link href="/info" className="animate-pulse text-white text-2xl font-bold px-3 py-1 rounded-2xl hover:bg-gray-800 transition-colors duration-200">?</Link>
